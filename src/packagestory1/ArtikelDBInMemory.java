78a53a2b650aed848450acc2db4f311e302f37ea
Phonkrit Van de Velde <Phonkrit.vandevelde@student.ucll.be>
2019-11-28 16:33:42 +0100

    packageStory 1 aangepast

--- src/packagestory1/ArtikelDBInMemory.java
+++ src/packagestory1/ArtikelDBInMemory.java
@@ -1,14 +1,31 @@
 package packagestory1;
 
+import model.Artikel;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ArtikelDBInMemory implements ArtikelDBStrategy {
-    //Hierin zit hashmap
+    //Hierin zit hashmap -lees uit, uit artikel.txt
     //Enum van types van  databases
     //Methode: Load en Save
     //Dit is nu de context klasse voor load en save strategy
     private LoadSaveStrategy loadSaveStrategy;
     private Map hashMap = new HashMap();
+
+    public ArtikelDBInMemory() {
+    }
+
+    public ArrayList<Artikel> load(){
+
+        return
+    }
+
+
+    public void save(ArrayList<Artikel> artikelArrayList){
+
+    }
+
 
 }
--- src/packagestory1/ArtikelDBStrategy.java
+++ src/packagestory1/ArtikelDBStrategy.java
@@ -1,8 +1,11 @@
 package packagestory1;
 
+import model.Artikel;
+
+import java.util.ArrayList;
+
 public interface ArtikelDBStrategy {
-
-
-
+    ArrayList<Artikel> load();
+    void save(ArrayList<Artikel> artikelArrayList);
 }
 
