84d309bfd9e127952f464aac5832bae137258902
Phonkrit Van de Velde <Phonkrit.vandevelde@student.ucll.be>
2019-12-02 14:02:18 +0100

    packageStory 1 verdergemaakt

--- src/packagestory1/ArtikelDBInMemory.java
+++ src/packagestory1/ArtikelDBInMemory.java
@@ -28,47 +28,11 @@ public class ArtikelDBInMemory implements ArtikelDBStrategy {
     }
 
     public ArrayList<Artikel> load(String bestand){
-        ArrayList a= new ArrayList<Artikel>();
-
-        if (bestand == null || bestand.trim().isEmpty()) throw new IllegalArgumentException("Artikel mag niet leeg zijn");
-        File artikelenFile = new File(bestand);
-        try {
-            Scanner scannerFile = new Scanner(artikelenFile);
-            while (scannerFile.hasNextLine()) {
-                Scanner scannerLijn = new Scanner(scannerFile.nextLine());
-                scannerLijn.useDelimiter(",");
-                String id = scannerLijn.next();
-                String omschrijving = scannerLijn.next();
-                String groep = scannerLijn.next();
-                String pris = scannerLijn.next();
-                double prijs = Double.parseDouble(pris);
-                String stok = scannerLijn.next();
-                int stock = Integer.parseInt(stok);
-                //vb: 1,artikel1,gr1,12.5,10
-                Artikel artikel = new Artikel(id, omschrijving, groep, prijs, stock);
-                a.add(artikel);
-            }
-            return a;
-
-            //artikelen = sortByValues(artikelen);
-        } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("Fout bij het inlezen", e);
-        }
 
     }
 
 
     public void save(ArrayList<Artikel> artikelArrayList){
-        File personenFile = new File("Personen.txt");
-        try{
-            PrintWriter writer = new PrintWriter(personenFile);
-            writer.println("Max " + "Van de Velde");
-            writer.println("Luca " + "VH");
-            writer.close();
-        } catch (FileNotFoundException e1) {
-            throw new IllegalArgumentException("Bestand niet gevonden" + e1);
-        }
-
 
     }
 
