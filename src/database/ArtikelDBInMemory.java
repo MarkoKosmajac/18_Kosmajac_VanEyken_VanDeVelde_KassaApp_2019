4bee2f3b108373ae96fce45251810964711792d3
Phonkrit Van de Velde <Phonkrit.vandevelde@student.ucll.be>
2019-12-05 20:56:35 +0100

    Template class afgewerkt

--- src/database/ArtikelDBInMemory.java
+++ src/database/ArtikelDBInMemory.java
@@ -5,7 +5,6 @@ import model.Artikel;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ArtikelDBInMemory implements ArtikelDBStrategy {
     //Hierin zit hashmap -lees uit, uit artikel.txt
@@ -14,7 +13,7 @@ public class ArtikelDBInMemory implements ArtikelDBStrategy {
     //Dit is nu de context klasse voor load en save strategy
     private LoadSaveStrategy loadSaveStrategy;
     private HashMap artikelen = new HashMap<String, Artikel>();
-    private TreeMap treeMap = new TreeMap<>(new ComparatorOpOmschrijving());
+    private TreeMap treeMap;
 
 
 
@@ -28,6 +27,39 @@ public class ArtikelDBInMemory implements ArtikelDBStrategy {
 
             }
         }
+        convertToTreeMap(artikelen);
+
+
+    }
+
+   /* public  <K, V> Map<K, V> convertToTreeMap(Map<K, V> hashMap)
+    {
+        // Create a new TreeMap
+        Map<K, V> treeMap = new TreeMap<>(new ComparatorOpOmschrijving());
+
+        // Pass the hashMap to putAll() method
+        treeMap.putAll(artikelen);
+
+        // Return the TreeMap
+        return treeMap;
+    } */
+
+    public  <K, V> void convertToTreeMap(Map<K, V> hashMap) {
+        SortedSet<Map.Entry<String, Artikel>> sortedset = new TreeSet<Map.Entry<String, Artikel>>(
+                new Comparator<Map.Entry<String, Artikel>>() {
+                    @Override
+                    public int compare(Map.Entry<String, Artikel> o1, Map.Entry<String, Artikel> o2) {
+                        String omschrijving1 = o1.getValue().getOmschrijving().toLowerCase();
+                        String omschrijving2 = o2.getValue().getOmschrijving().toLowerCase();
+
+                        return omschrijving2.compareTo(omschrijving1);
+                    }
+
+                });
+
+        sortedset.addAll(artikelen.entrySet());
+
+
     }
 
 
