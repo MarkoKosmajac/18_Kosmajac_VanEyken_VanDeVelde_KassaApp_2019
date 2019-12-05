68fbb6a807ff665abcabe96bfd7ce9cd1401fb9a
MarkoKosmajac <marko.kosmajac@student.ucll.be>
2019-12-05 19:50:22 +0100

    JavaFX Story 2 Done (Instellingen Tab) (Used ENUM)

--- src/view/panels/InstellingenPane.java
+++ src/view/panels/InstellingenPane.java
@@ -1,17 +1,22 @@
 package view.panels;
 
+/**
+ * @author Marko Kosmajac
+ */
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import model.Artikel;
 import model.ArtikelCompany;
+import model.SoortBestand;
 
-public class InstellingenPane extends GridPane { //TODO: Simplify code by letting panes extend a Pane (Here gridpane)!
-    //private TableView<Artikel> table ;//TODO: alle veldjes, rijen, uit de tekstfile....
-    private ArtikelCompany artikelCompany ;
+public class InstellingenPane extends GridPane {
+    private ArtikelCompany artikelCompany;
 
     public InstellingenPane(ArtikelCompany artikelCompany){
         this.artikelCompany = artikelCompany;
@@ -20,22 +25,27 @@ public class InstellingenPane extends GridPane { //TODO: Simplify code by lettin
         this.setVgap(5);
         this.setHgap(5);
 
-        this.add(new Label("Opties:"), 0, 0, 1, 1);
+        this.add(new Label("Opties:"), 0, 0);
 
         final ToggleGroup group = new ToggleGroup();
 
         RadioButton rb1 = new RadioButton("Relationele Database");
         rb1.setToggleGroup(group);
-        rb1.setSelected(true);
+        //rb1.setSelected(true);
 
         RadioButton rb2 = new RadioButton("In Memory Database");
         rb2.setToggleGroup(group);
 
+        this.add(new Label("Indien gekozen voor 'In Memory Database' "),0,3);
+
+        ComboBox<SoortBestand> comboBoxx = new ComboBox<>();
+        comboBoxx.getItems().setAll(SoortBestand.values());
 
         this.add(rb1,0,1);
         this.add(rb2,0,2);
+        this.add(comboBoxx,0,4);
 
-        //this.getChildren().addAll(lblHeading, table); //button, button2
 
     }
+
 }
