da4e6b9b639f594c3736089d8495cc71517f02c7
Phonkrit Van de Velde <Phonkrit.vandevelde@student.ucll.be>
2019-12-06 15:17:01 +0100

    Properties file gemaakt en afgewerkt

--- src/view/panels/InstellingenPane.java
+++ src/view/panels/InstellingenPane.java
@@ -15,8 +15,13 @@ import model.Artikel;
 import model.ArtikelCompany;
 import model.SoortBestand;
 
+import javax.swing.*;
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InstellingenPane extends GridPane {
     private ArtikelCompany artikelCompany;
@@ -24,11 +29,9 @@ public class InstellingenPane extends GridPane {
     public InstellingenPane(ArtikelCompany artikelCompany) throws IOException {
 
         Properties properties = new Properties();
-        InputStream is = new FileInputStream("src\\database\\KassaApp.properties");
-        FileOutputStream os = new FileOutputStream("src\\database\\KassaApp.properties");
-        String em = properties.getProperty("databaseInMemory");
 
         this.artikelCompany = artikelCompany;
+
 
         this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
@@ -38,7 +41,7 @@ public class InstellingenPane extends GridPane {
 
         final ToggleGroup group = new ToggleGroup();
 
-        RadioButton rb1 = new RadioButton("Relationele Database");
+        RadioButton rb1 = new RadioButton(" Relationele Database");
         rb1.setToggleGroup(group);
         //rb1.setSelected(true);
 
@@ -49,6 +52,64 @@ public class InstellingenPane extends GridPane {
 
         ComboBox<SoortBestand> comboBoxx = new ComboBox<>();
         comboBoxx.getItems().setAll(SoortBestand.values());
+
+
+        group.selectedToggleProperty().addListener((ov, old_toggle, new_toggle) -> {
+            FileOutputStream os = null;
+
+            if (group.getSelectedToggle() == rb2) {
+                try {
+                    os = new FileOutputStream("src" + File.separator + "database" + File.separator + "KassaApp.properties");
+                    properties.clear();
+                    properties.setProperty("databaseInMemory", "databaseInMemory");
+
+                    properties.store(os,null);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            if (group.getSelectedToggle() == rb1) {
+
+                try {
+                    os = new FileOutputStream("src" + File.separator + "database" + File.separator + "KassaApp.properties");
+                    properties.clear();
+                    properties.setProperty("deandere", "deandere");
+
+                    properties.store(os,null);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+
+
+        InputStream in = new FileInputStream(new File("src" + File.separator + "database" + File.separator + "KassaApp.properties"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        this.add(new Label(out.toString()), 0, 12);
+        if (out.toString().contains("deandere=deandere")){
+            rb1.setSelected(true);
+        } else if (out.toString().contains("databaseInMemory=databaseInMemory")){
+            rb2.setSelected(true);
+        }
+
+
+        //out.toString()); Prints the string content read from input stream
+        reader.close();
+
+
+
+
+
 
 
         this.add(rb1,0,1);
