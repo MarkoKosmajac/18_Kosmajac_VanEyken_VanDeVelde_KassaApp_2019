124c354d32a1ccab52ba305051ffcbf8909b349f
MarkoKosmajac <marko.kosmajac@student.ucll.be>
2019-12-08 00:06:59 +0100

    Story 2: Factory refactoring

--- src/database/LoadSaveStrategyFactory.java
+++ src/database/LoadSaveStrategyFactory.java
@@ -3,6 +3,7 @@ package database;
 public class LoadSaveStrategyFactory {
 
     //TODO: Is het zo juist met equalsIgnoreCase ?...
+    //TODO: NOG TE IMPLEMENTERE IN ArtikelDBContext etc... Singleton, reflection,...
     public LoadSaveStrategy makeLoadSaveStrategy(String loadSaveStrategy){
         LoadSaveStrategy loadSaveStrategyf = null;
         if(loadSaveStrategy.equalsIgnoreCase("ArtikelLoadSaveTekst")){
