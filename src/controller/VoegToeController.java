ed36f93bb971aec1ea9e304f43f3b7fa88d05f6a
MarkoKosmajac <marko.kosmajac@student.ucll.be>
2019-12-08 22:04:33 +0100

    MVC - Part 1 (extra)

---
+++ src/controller/VoegToeController.java
@@ -0,0 +1,73 @@
+package controller;
+
+import database.ArtikelDBContext;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import view.panels.KassaTab1OverviewPane;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.security.Key;
+
+public class VoegToeController {
+    private KassaTab1OverviewPane theView;
+    private ArtikelDBContext theModel;
+
+    public VoegToeController(KassaTab1OverviewPane theView, ArtikelDBContext theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+
+        this.theView.addVoegToeHandler(new addVoegToeHandler());
+    }
+
+    //class addVoegToeHandler implements EventHandler<ActionEvent> {
+    class addVoegToeHandler implements javafx.event.EventHandler<javafx.scene.input.KeyEvent>{
+
+        /*@Override
+        public void handle(KeyEvent e) {
+            String code = "";
+            // Surround interactions with the view with
+            // a try block in case numbers weren't
+            // properly entered
+            try{
+                if (e.getKeyCode() == KeyCode.ENTER.getCode()){
+                code = theView.getIngevuldeWaarde();
+                theModel.zoekCodeOp(code);
+                theView.setWaarde(theModel.getOpgezochteCode());*/
+                //TODO: OOK ERGENS HIER, er mag in de view niet gerekent worden!!!!
+                /*String invoer = text.getText();
+                String em = artikelDBContext.zoek(invoer);
+                Artikel artikel = artikelDBContext.zoekArtikel(invoer);
+                if(em.contains(",")){ //PRODUCT = FOUND
+                    String omschr = artikel.getOmschrijving();
+                    double prijs = artikel.getPrijs();
+                    totaalBedrag += artikel.getPrijs();
+                    //System.out.println("Omschrijving: " + omschr + " Prijs: " + prijs + " totaal: " + totaalBedrag);
+                    products.add(artikel);
+                    table.setItems(products);
+                    text.clear();*/
+                /*}
+            }
+            catch(NumberFormatException ex){
+                theView.displayErrorMessage("Niet gevonden.");
+            }
+        }*/
+
+        @Override
+        public void handle(javafx.scene.input.KeyEvent event) {
+            try{
+                String code = "";
+                if (event.getCode() == KeyCode.ENTER){
+                    code = theView.getIngevuldeWaarde();
+                    theModel.zoekCodeOp(code);
+                    theView.setWaarde(theModel.getOpgezochteCode());
+                }
+            }
+            catch(NumberFormatException ex){
+            theView.displayErrorMessage("Niet gevonden.");
+            }
+        }
+    }
+
+}
