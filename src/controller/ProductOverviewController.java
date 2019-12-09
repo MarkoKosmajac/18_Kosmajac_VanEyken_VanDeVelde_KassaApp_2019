f18514797971b7dd30df271fbd35d210a6220916
MarkoKosmajac <marko.kosmajac@student.ucll.be>
2019-12-09 15:35:02 +0100

    Controller + PaneOverview

--- src/controller/ProductOverviewController.java
+++ src/controller/ProductOverviewController.java
@@ -2,13 +2,35 @@ package controller;
 
 import model.Artikel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductOverviewController {
 
+    List<Artikel> winkelmandje;
+
+    public ProductOverviewController() {
+        winkelmandje = new ArrayList<>();
+    }
+
+    //TODO: MODELKLASSE ?
     public Artikel getArtikel(String ingevuldeWaarde) {
-        return null; //TODO:
+        Artikel a = null;
+        for(Artikel artikel : winkelmandje){
+            if(artikel.getArtikelCode().equalsIgnoreCase(ingevuldeWaarde)){
+                a = artikel;
+            }
+        }
+        if(a == null){
+            throw new ControllerException("Geen artikel gevonden.");
+        }else{
+            return a;
+        }
     }
 
     public void addToWinkelMandje(Artikel artikel) {
-        //TODO:
+        winkelmandje.add(artikel);
+        System.out.println(artikel);
+        //TODO: IETS
     }
 }
