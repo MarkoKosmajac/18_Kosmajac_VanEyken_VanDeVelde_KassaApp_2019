b7a9c3b39d5fdaede27ade1aa3c9faba6a02c770
Phonkrit Van de Velde <Phonkrit.vandevelde@student.ucll.be>
2019-12-15 22:09:45 +0100

    CSS

--- src/view/KassaView.java
+++ src/view/KassaView.java
@@ -20,6 +20,7 @@ public class KassaView {
 		borderPane.prefHeightProperty().bind(scene.heightProperty());
 		borderPane.prefWidthProperty().bind(scene.widthProperty());
 		root.getChildren().add(borderPane);
+		scene.getStylesheets().add("application.css");
 		stage.setScene(scene);
 		stage.sizeToScene();			
 		stage.show();		
